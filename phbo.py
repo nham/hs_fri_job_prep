1cf33ba64d106e5f616f457aedc1f5c64a8adc39
nham <hamann.nick@gmail.com>
2014-06-20 15:34:51 -0400

    Start an attempt to rip out the exceptions and use a kind of Either type for return values. Setting aside for now, not sure this is idiomatic Python

--- phbo.py
+++ phbo.py
@@ -8,12 +8,24 @@ class PhonebookException(Exception):
     def __init__(self, value):
         self.value = value
 
-
 class ArgParseException(PhonebookException):
     pass
 
-class CommandException(PhonebookException):
-    pass
+
+def ok(v):
+    return (v, None)
+
+# The only problem is that None is not a valid value.
+def err(v):
+    return (None, v);
+
+def is_err(v):
+    if not isinstance(v, tuple) or len(v) != 2:
+        return False
+
+    if v[0] is None:
+        return True
+
 
 
 def init_db():
@@ -52,6 +64,7 @@ class PhonebookManager:
         else:
             return result[0]
 
+
     # checks if <name> is already in the phonebook associated with <pbid>
     def name_in_list(self, name, pbid):
         c = self.conn.cursor()
@@ -61,99 +74,120 @@ class PhonebookManager:
         return result is not None
 
 
+    # Returns (True, None) on success or (None, <string>) on failure
     def create_phonebook(self, pb_name):
         if self.pb_exists(pb_name):
-            raise CommandException("Cannot create phonebook '{}' because it already exists".format(pb_name))
+            return err("Cannot create phonebook '{}' because it already exists".format(pb_name))
 
         c = self.conn.cursor()
         sql = "INSERT INTO phonebooks (name) VALUES (?)"
         c.execute(sql, (pb_name,))
         self.conn.commit()
 
+        return ok(True)
 
+
+    # Returns a (<list>, None) on success, or (None, <string>) on failure
     def lookup_name(self, name, pb_name):
         pbid = self.pb_exists(pb_name)
         if not pbid:
-            raise CommandException("Phonebook '{}' does not exist".format(pb_name))
+            return err("Phonebook '{}' does not exist".format(pb_name))
 
         c = self.conn.cursor()
         sql = "SELECT name, number FROM pb_entries WHERE pbid = ? and name LIKE ?"
         c.execute(sql, (pbid, '%'+name+'%'))
 
-        result = c.fetchone()
-        while result != None:
-            self.display_entry(result[0], result[1])
-            result = c.fetchone()
+        sql_result = c.fetchone()
+        result = []
+        while sql_result != None:
+            result.append( self.format_entry(sql_result[0], sql_result[1]) )
+            sql_result = c.fetchone()
+
+        return ok(result)
 
 
+
+    # Returns (True, None) on success or (None, <string>) on failure
     def add_entry(self, name, number, pb_name):
         pbid = self.pb_exists(pb_name)
         if not pbid:
-            raise CommandException("Phonebook '{}' does not exist".format(pb_name))
+            return err("Phonebook '{}' does not exist".format(pb_name))
 
         if self.name_in_list(name, pbid):
-            raise CommandException("'{}' is already added to phonebook '{}'".format(name, pb_name))
-
+            return err("'{}' is already added to phonebook '{}'".format(name, pb_name))
 
         c = self.conn.cursor()
         sql = "INSERT INTO pb_entries (name, number, pbid) VALUES (?, ?, ?)"
         c.execute(sql, (name, number, pbid))
         self.conn.commit()
 
+        return ok(True)
 
+
+    # Returns (True, None) on success or (None, <string>) on failure
     def change_entry(self, name, number, pb_name):
         pbid = self.pb_exists(pb_name)
         if not pbid:
-            raise CommandException("Phonebook '{}' does not exist".format(pb_name))
+            return err("Phonebook '{}' does not exist".format(pb_name))
 
         if not self.name_in_list(name, pbid):
-            raise CommandException("'{}' isn't in phonebook '{}', so it cannot be changed.".format(name, pb_name))
+            return err("'{}' isn't in phonebook '{}', so it cannot be changed.".format(name, pb_name))
 
         c = self.conn.cursor()
         sql = "UPDATE pb_entries SET number = ? WHERE pbid=? and name=?"
         c.execute(sql, (number, pbid, name))
         self.conn.commit()
 
+        return ok(True)
 
 
+
+    # Returns (True, None) on success or (None, <string>) on failure
     def remove_entry(self, name, pb_name):
         pbid = self.pb_exists(pb_name)
         if not pbid:
-            raise CommandException("Phonebook '{}' does not exist".format(pb_name))
+            return err("Phonebook '{}' does not exist".format(pb_name))
 
         if not self.name_in_list(name, pbid):
-            raise CommandException("'{}' isn't in phonebook '{}', so it cannot be removed.".format(name, pb_name))
+            return err("'{}' isn't in phonebook '{}', so it cannot be removed.".format(name, pb_name))
 
         c = self.conn.cursor()
         sql = "DELETE FROM pb_entries WHERE name = ? and pbid = ?"
         c.execute(sql, (name, pbid))
         self.conn.commit()
+
+        return ok(True)
         
 
 
+    # Returns a (<list>, None) on success or (None, <string>) on failure
     def reverse_lookup(self, number, pb_name):
         pbid = self.pb_exists(pb_name)
         if not pbid:
-            raise CommandException("Phonebook '{}' does not exist".format(pb_name))
+            return err("Phonebook '{}' does not exist".format(pb_name))
 
         c = self.conn.cursor()
         sql = "SELECT name, number FROM pb_entries WHERE pbid = ? and number = ?"
         c.execute(sql, (pbid, number))
 
-        result = c.fetchone()
-        while result != None:
-            self.display_entry(result[0], result[1])
-            result = c.fetchone()
+        sql_result = c.fetchone()
+        result = []
+        while sql_result != None:
+            result.append( self.format_entry(sql_result[0], sql_result[1]) )
+            sql_result = c.fetchone()
+
+        return ok(result)
 
 
-    def display_entry(self, name, number):
-        print("{}: {}".format(name, number))
+
+    def format_entry(self, name, number):
+        return "{}: {}".format(name, number)
 
 
 
 def exec_command(args, default_phonebook=None):
     if len(args) == 0:
-        raise ArgParseException("No command specified.")
+        return err("No command specified.")
 
     # create <phonebook>
     # lookup <name> <phonebook>
@@ -165,20 +199,27 @@ def exec_command(args, default_phonebook=None):
     def check_num_params(cmd, num, params):
         num_params = len(params)
         if num_params != num:
-            raise ArgParseException("Incorrect number of parameters for '{}', should be {}, but {} were given".format(cmd, num, num_params))
+            return err("Incorrect number of parameters for '{}', should be {}, but {} were given".format(cmd, num, num_params))
+        else:
+            return ok(True)
 
 
     # with the default_phonebook option, we have to figure out whether or not to use it
     # (we may be overriding the default by supplying an actual phonebook)
     def create_params_list(cmd, num, params):
-        try:
-            check_num_params(cmd, num, params)
-            return params
-        except ArgParseException as e:
+        check = check_num_params(cmd, num, params)
+
+        if is_err(check):
             if default_phonebook is not None:
                 params_w_default = params + [default_phonebook]
-                check_num_params(cmd, num, params_w_default)
-                return params_w_default
+                check = check_num_params(cmd, num, params_w_default)
+
+                if is_err(check):
+                    return check
+                else:
+                    return ok(params_w_default)
+        else:
+            return ok(params)
 
 
     cmd = args[0] # the main command
@@ -189,36 +230,36 @@ def exec_command(args, default_phonebook=None):
 
     if cmd == 'create':
         check_num_params('create', 1, cmd_params)
-        pbm.create_phonebook(cmd_params[0])
+        return pbm.create_phonebook(cmd_params[0])
 
     elif cmd == 'lookup':
         params = create_params_list('lookup', 2, cmd_params)
-        pbm.lookup_name(params[0], params[1])
+        return pbm.lookup_name(params[0], params[1])
 
     elif cmd == 'add':
         params = create_params_list('add', 3, cmd_params)
-        pbm.add_entry(params[0], params[1], params[2])
+        return pbm.add_entry(params[0], params[1], params[2])
 
     elif cmd == 'change':
         params = create_params_list('change', 3, cmd_params)
-        pbm.change_entry(params[0], params[1], params[2])
+        return pbm.change_entry(params[0], params[1], params[2])
 
     elif cmd == 'remove':
         params = create_params_list('remove', 2, cmd_params)
-        pbm.remove_entry(params[0], params[1])
+        return pbm.remove_entry(params[0], params[1])
 
     elif cmd == 'reverse-lookup':
         params = create_params_list('reverse-lookup', 2, cmd_params)
-        pbm.reverse_lookup(params[0], params[1])
+        return pbm.reverse_lookup(params[0], params[1])
 
     else:
         # try defaulting to 'lookup' command before failing
         try:
             params = create_params_list('lookup', 2, args)
         except Exception as e:
-            raise ArgParseException("Invalid command.")
+            err("Invalid command.")
 
-        pbm.lookup_name(params[0], params[1])
+        return pbm.lookup_name(params[0], params[1])
 
 
 
@@ -227,13 +268,20 @@ if __name__ == "__main__":
         # check if there's a config file first
         config = configparser.ConfigParser()
         config.read("phbo.cfg")
-        if 'default_phonebook' in config['DEFAULT']:
-            exec_command(sys.argv[1:], default_phonebook=config['DEFAULT']['default_phonebook'])
-        else:
-            exec_command(sys.argv[1:])
 
-    except PhonebookException as e:
-        print("Error: " + e.value)
+        if 'default_phonebook' in config['DEFAULT']:
+            default_pb = config['DEFAULT']['default_phonebook']
+        else:
+            default_pb = None
+
+        result = exec_command(sys.argv[1:], default_phonebook=default_pb)
+
+        if is_err(result):
+            print("Error: " + result[1])
+        elif isinstance(result[0], list):
+            for line in result[0]:
+                print(line)
+
     except Exception as e:
         print(e)
 
