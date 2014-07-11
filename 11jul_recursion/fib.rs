09e276e86cac4c1869f199b161fe2575657f726c
nham <hamann.nick@gmail.com>
2014-07-11 17:02:27 -0400

    Add version using an Iterator

--- 11jul_recursion/fib.rs
+++ 11jul_recursion/fib.rs
@@ -40,6 +40,34 @@ impl FibMemoizer {
     }
 }
 
+struct FibIter {
+    prev: uint,  // n - 1
+    prev2: uint, // n - 2
+}
+
+impl FibIter {
+    fn new() -> FibIter {
+        FibIter { prev: 1u, prev2: 0u }
+    }
+}
+
+impl Iterator<uint> for FibIter {
+    fn next(&mut self) -> Option<uint> {
+        let tmp = self.prev2;
+        self.prev2 = self.prev;
+        self.prev = tmp + self.prev2;
+        Some(tmp)
+    }
+}
+
+fn fib_iter(n: uint) -> uint {
+    let mut it = FibIter::new();
+    let mut x = 0u;
+    for i in range(0, n+1) { x = it.next().unwrap(); }
+    x
+}
+
+
 #[bench]
 fn bench_naive(b: &mut Bencher) {
     b.iter(|| naive_fib(25) );
@@ -51,10 +79,16 @@ fn bench_memoizing(b: &mut Bencher) {
     b.iter(|| fm.fib(25) );
 }
 
+#[bench]
+fn bench_iter(b: &mut Bencher) {
+    let mut fm = FibMemoizer::new();
+    b.iter(|| fib_iter(25) );
+}
+
 fn main() {
     let mut fm = FibMemoizer::new();
-
     for i in range(0u, 20) {
-        println!("Naive: {}, Memoizing: {}", naive_fib(i), fm.fib(i));
+        println!("Naive: {}, Memoizing: {}, Iter: {}", 
+                 naive_fib(i), fm.fib(i), fib_iter(i));
     }
 }
